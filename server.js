3fe1682763ed2f1120efe188724a14ad540d2c71
j1ands <jordanlandau1@gmail.com>
2017-09-25 16:45:21 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const server = express();
+
+server.get("*", function (req, res) {
+  res.send("Hello World!")
+});
+
+server.listen(8080, function () {
+  console.log("Example app listening on port 8080!")
+});
