f0a26c57c87d53212c26fccedd211b335f365448
j1ands <jordanlandau1@gmail.com>
2017-09-25 16:53:50 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const server = express();
 
+server.set("port", (process.env.PORT || 5000));
+
 server.get("*", function (req, res) {
   res.send("Hello World!")
 });
 
-server.listen(5000, function () {
-  console.log("Example app listening on port 5000!")
+server.listen(server.get("port"), function () {
+  console.log("Example app listening on port ", server.get("port"));
 });
